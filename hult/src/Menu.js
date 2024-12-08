2d988ecfa48d55230af22b15b750556c0d9f0b12
Taha Assadini <118551860+Assadini@users.noreply.github.com>
2024-12-08 15:38:52 +0000

    l

---
+++ hult/src/Menu.js
@@ -0,0 +1,21 @@
+import './menu.css';
+import logo from './img/logo.png';
+
+function Menu() {
+    return (
+      <div className="menu">
+        <img src={logo} alt="hult prize logo"></img>
+        <ul>
+          
+          <li><a href='#'>About us</a></li>
+          <li><a href='#'>How it works</a></li>
+          <li><a href='#'>Hult prize winners</a></li>
+          <li><a href='#'>Stories</a></li>
+          <li className='no-wrap'><a href='#'>Apply now</a></li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Menu;
+  
